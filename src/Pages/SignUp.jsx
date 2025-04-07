7ffa7fa33fa9757fc2619b6666995b495edb0201
MarcosFig-dev <marcos.amfig@gmail.com>
2025-04-08 08:52:16 +1200

    Created signup page
    created signup page and linked supabase to porject

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,7 +1,72 @@
-import React from 'react';
+import { useState } from "react";
+import { supabase } from "../supabaseClient";
+import './SignUp.css';
 
 function SignUp() {
-  return <h2>Sign Up Page</h2>;
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSignup = async () => {
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        data: {
+          first_name: firstName,
+          last_name: lastName
+        },
+      },
+    });
+
+    if (error) {
+      setMessage(error.message)
+    }
+    else {
+      setMessage('Check your email to confirm sign up!')
+    }
+  }
+
+  return (
+    <div className="signup-container">
+      <h2 className="signup-title">Sign Up</h2>
+      <div className="signup=form">
+        <input
+          className="signup-input"
+          type="text"
+          placeholder="First Name"
+          value={firstName}
+          onChange={e => setFirstName(e.target.value)}
+        />
+        <input
+          className="signup-input"
+          type="text"
+          placeholder="Last Name"
+          value={lastName}
+          onChange={e => setLastName(e.target.value)}
+        />
+        <input
+          className="signup-input"
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+        <input
+          className="signup-input"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <br />
+        <button className="signup-button" onClick={handleSignup}>Sign Up</button>
+        {message && <p className="signup-message">{message}</p>}
+      </div>
+    </div>
+  );
 }
 
-export default SignUp;
+export default SignUp
