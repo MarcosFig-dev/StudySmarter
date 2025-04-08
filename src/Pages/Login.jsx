fa5d282986fedae3132505fdc0c8d107fdf59843
MarcosFig-dev <marcos.amfig@gmail.com>
2025-04-08 20:41:48 +1200

    Created login page
    Created login page, renamed home page to landing page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,50 @@
-import React from 'react';
+import { useState } from "react";
+import { supabase } from "../supabaseClient";
+import './Login.css';
 
 function Login() {
-  return <h2>Login Page</h2>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleLogin = async () => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      setMessage(error.message);
+    }
+    else {
+      setMessage("Successfully logged in!");
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2 className="login-title">Login</h2>
+      <div className="login-form">
+        <input
+          className="login-input"
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+        <input
+          className="login-input"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <br />
+        <button className="login-button" onClick={handleLogin}>Login</button>
+        {message && <p className="login-message">{message}</p>}
+      </div>
+    </div>
+  )
 }
 
 export default Login;
